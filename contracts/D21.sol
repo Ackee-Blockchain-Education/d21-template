8348571c0ff2f80388760bfc558285b228fa4895
honzaprevratil <honzikprevratil@gmail.com>
2025-10-16 15:29:43 +0200

    ✨ Update for 2025

--- contracts/D21.sol
+++ contracts/D21.sol
@@ -4,42 +4,60 @@ pragma solidity 0.8.26;
 import "./IVoteD21.sol";
 
 contract D21 is IVoteD21 {
-    // State variables
 
-    // Custom errors
+    // --------------------------------- State ---------------------------------
+    // TODO: Addstate variables
 
-    // Functions
+    // -------------------------------- Errors --------------------------------
+    // TODO: Custom errors
 
-    /// @inheritdoc IVoteD21
-    function addSubject(string memory name_) external {}
-
-    /// @inheritdoc IVoteD21
-    function getSubjects() external view returns (address[] memory) {}
+    // ---------------------- Public & external functions ---------------------
 
     /// @inheritdoc IVoteD21
-    function getSubject(address addr_) external view returns (Subject memory) {}
+    function addSubject(string memory name) external {}
 
     /// @inheritdoc IVoteD21
-    function addVoter(address voter_) external {}
+    function getSubjects() external view returns (address[] memory) {
+        // TODO: Implement
+    }
 
     /// @inheritdoc IVoteD21
-    function startVoting() external {}
+    function getSubject(address subject) external view returns (Subject memory) {
+        // TODO: Implement
+    }
 
     /// @inheritdoc IVoteD21
-    function votePositive(address subject_) external {}
+    function addVoter(address voter) external {
+        // TODO: Implement
+    }
 
     /// @inheritdoc IVoteD21
-    function voteNegative(address subject_) external {}
+    function startVoting() external {
+        // TODO: Implement
+    }
 
     /// @inheritdoc IVoteD21
-    function voteBatch(
-        address[] calldata subjects_,
-        bool[] calldata votes_
-    ) external {}
+    function votePositive(address subject) external {
+        // TODO: Implement
+    }
 
     /// @inheritdoc IVoteD21
-    function getRemainingTime() external view returns (uint256) {}
+    function voteNegative(address subject) external {
+        // TODO: Implement
+    }
 
     /// @inheritdoc IVoteD21
-    function getResults() external view returns (Subject[] memory) {}
+    function voteBatch(address[] calldata subjects, bool[] calldata votes) external {
+        // TODO: Implement
+    }
+
+    /// @inheritdoc IVoteD21
+    function getRemainingTime() external view returns (uint256) {
+        // TODO: Implement
+    }
+
+    /// @inheritdoc IVoteD21
+    function getResults() external view returns (Subject[] memory) {
+        // TODO: Implement
+    }
 }
--- contracts/IVoteD21.sol
+++ contracts/IVoteD21.sol
@@ -1,99 +1,81 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
+pragma solidity ^0.8.26;
 
-struct Subject {
-    string name;
-    int256 votes;
-}
-
+/// @notice Interface for a D21 voting system
 interface IVoteD21 {
+
+    // ------------------------------ Structures ------------------------------
+
+    /// @notice The subject of voting – can be for example a political party.
+    struct Subject {
+        string name;
+        int256 votes;
+    }
+
+    // -------------------------------- Events --------------------------------
+
+    /// @notice Emmited when the owner started the voting period.
     event VotingStarted();
+
+    /// @notice Emmited when the owner added a new voter.
+    /// @param voter  Address of the added voter
     event VoterAdded(address indexed voter);
-    event SubjectAdded(address indexed addr, string name);
+
+    /// @notice Emmited when a new subject was added.
+    /// @param subject  Address of the subject
+    /// @param name     Name of the subject
+    event SubjectAdded(address indexed subject, string name);
+
+    /// @notice Emmited when a positive vote was given.
+    /// @param voter    Address of the voter
+    /// @param subject  Address of the subject
     event PositiveVoted(address indexed voter, address indexed subject);
+
+    /// @notice Emmited when a negative vote was given.
+    /// @param voter    Address of the voter
+    /// @param subject  Address of the subject
     event NegativeVoted(address indexed voter, address indexed subject);
 
-    /**
-     * @notice UC1. Add a new subject to the voting.
-     *         UC7. Subjects can't be registered after the voting has started.
-     * @dev Emits the SubjectAdded event. One EOA cannot deploy more than one subject.
-     * @param name_ Subject name. Non-empty string.
-     */
-    function addSubject(string memory name_) external;
+    // ---------------------- Public & external functions ---------------------
 
-    /**
-     * @notice UC2. List all registered subjects.
-     * @return List of subject addresses.
-     */
-    function getSubjects() external view returns (address[] memory);
+    /// @notice Add a new subject into the voting system using the name.
+    /// @param name  Name of the subject
+    function addSubject(string memory name) external;
 
-    /**
-     * @notice UC3. Get subject details and results.
-     * @param addr_ Subject address.
-     * @return Subject details.
-     */
-    function getSubject(address addr_) external view returns (Subject memory);
+    /// @notice Get the subject details.
+    /// @param subject  Address of the subject
+    /// @return         Subject details
+    function getSubject(address subject) external view returns(Subject memory);
 
-    /**
-     * @notice UC4. Add a new voter to the voting.
-     * @dev Voters can only be added by the owner. Emits the VoterAdded event.
-     * @param voter_ Voter address.
-     */
-    function addVoter(address voter_) external;
+    /// @notice Get addresses of all registered subjects.
+    /// @return  Array of subject addresses
+    function getSubjects() external view returns(address[] memory);
 
-    /**
-     * @notice UC5. Start the voting. Voting ends after 4 days from the voting start.
-     * @dev Only the owner can start the voting. Emits the VotingStarted event.
-     */
+    /// @notice Add a new voter into the voting system.
+    /// @param voter  Address of the voter
+    function addVoter(address voter) external;
+
+    /// @notice Start the voting period.
     function startVoting() external;
 
-    /**
-     * @notice UC8. Vote positive for a subject. A voter has three positive votes.
-     *         UC9. Cannot vote for the same subject twice.
-     *         New votes are not accepted after the voting ends.
-     *         Votes are not accepted before the election starts.
-     * @dev Emits the PositiveVoted event.
-     * @param subject_ Subject address.
-     */
-    function votePositive(address subject_) external;
+    /// @notice Vote positive for the subject.
+    /// @param subject  Address of the subject
+    function votePositive(address subject) external;
 
-    /**
-     * @notice UC8. Vote positive for a subject. A voter has one negative vote.
-     *         UC9. Cannot vote for the same subject twice.
-     *         UC10. Negative vote can be used only after 2 positive votes.
-     *         New votes are not accepted after the voting ends.
-     *         Votes are not accepted before the election starts.
-     * @dev Emits the NegativeVoted event.
-     * @param subject_ Subject address.
-     */
-    function voteNegative(address subject_) external;
+    /// @notice Vote negative for the subject.
+    /// @param subject  Address of the subject
+    function voteNegative(address subject) external;
 
-    /**
-     * @notice UC11. Voters should be able to vote in one transaction.
-     * @param subjects_ List of subject addresses for voting.
-     * @param votes_ List of votes. True for a positive vote, false for a negative vote.
-     * @dev Votes are processed in the order of the subjects list.
-     *      The lists must have the same length.
-     *      The lenght may not exceed the number of allowed votes left for the voter.
-     *      Every vote emits the PositiveVoted or NegativeVoted event.
-     *      In case of a failure, the function reverts.
-     */
-    function voteBatch(
-        address[] calldata subjects_,
-        bool[] calldata votes_
-    ) external;
+    /// @notice Vote for multiple subjects.
+    /// @param subjects  Array of subject addresses
+    /// @param votes     Array of votes (true for positive, false for negative)
+    function voteBatch(address[] calldata subjects, bool[] calldata votes) external;
 
-    /**
-     * @notice Get the remaining time for the voting to end.
-     * @dev If the voting is not started, return 0.
-     * @return Remaining time in seconds.
-     */
-    function getRemainingTime() external view returns (uint256);
+    /// @notice Get the remaining time to the voting end in seconds.
+    /// @return  Remaining time in seconds
+    function getRemainingTime() external view returns(uint256);
 
-    /**
-     * @notice Get the voting results.
-     * @dev Results are sorted by the number of votes in descending order.
-     * @return List of subjects with their votes.
-     */
-    function getResults() external view returns (Subject[] memory);
+    /// @notice Get the voting results, sorted descending by votes.
+    /// @return  Array of subjects sorted by votes
+    function getResults() external view returns(Subject[] memory);
 }
